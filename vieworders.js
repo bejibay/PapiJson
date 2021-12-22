25f86b714422b5847338dfc4b2c8ad731c1fd2e7
bejibay <58639255+bejibay@users.noreply.github.com>
2021-12-23 00:54:34 +0100

    Update vieworders.js

--- vieworders.js
+++ vieworders.js
@@ -5,7 +5,7 @@ var xhr, obj, table, text, parsejson;
 //request data from vieworders.php file using ajax 
 //and convert json data to Javascript object
 
-funclon loadOrders(){
+function loadOrders(){
 xhr= new XLMHttpRequest;
 xhr.onload=function(){
 obj= this.responseText;}
@@ -15,12 +15,12 @@ xhr.send();
 
 //convert json array string data to Javascript object
 parsejson= JSON.parse(obj);
-table="<table>;
+table="<table>";
 table+="<tr><th>Product Name</th><th><Product Quantity</th><th>Price</th><th>Value</th><th>Date</th></tr>";
-for(1=0; 1< parsejson.length; 1++){
+for(i=0; i< parsejson.length; i++){
 text=parsejson[i];
 table+="<tr><td>text.name</td><td>text.quantity</td><td>text.price</td><td>text.value</td><td>text.date</td></td>";
-table+="</table>"';
+table+="</table>";
 document.getElementById ("display"),innerHTML=table;
 
 
