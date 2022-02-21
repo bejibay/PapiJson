7e9a1c8d020e8dc2d6923ca79a66d5df94d57886
bejibay <58639255+bejibay@users.noreply.github.com>
2022-02-21 13:07:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,36 @@
 <?php
+//function to validate input data
+
+function testdata($data){
+$data=trim($data);
+$data=stripslashes($data);
+$data=htmlspecialchars($data);
+return $data;
+}
+
 // collect form fields data
 if($_SERVER['REQUEST_METHOD']=='POST'){
-$name=testinput($_POST['name']);
-$quantity=testinput($_POST['quantity']);
-$price=testinput($_POST['price']);
+if(!empty($name)&&(is_int($name))$name=testinput($_POST['name']);
+if(!empty($quantity)&&(is_int($quantity))$quantity=testinput($_POST['quantity']);
+if(!empty($price)&&(is_int($price))$price=testinput($_POST['price']);
 $value=$quantity*$price;
-$value=testinput($_POST['value']);
-$orderdate=testinput($_POST['orderdate']);
+if(!empty($value)&&(is_int($value))$value=testinput($_POST['value']);
+if(!empty($orderdate))$orderdate=testinput($_POST['orderdate']);
 $submit=$_POST['submit'];
+
+//declare an array to store each form post
+$data = array();
+
 //put form fields into array'
+
 $formdata=array('name' =>$name, 'quantity'=>$quantity, 'price'=>$price, 'value'=>$value, 'orderdate'=>$orderdate);
 
+$data = array_push($data,$formdata);
+
 //convert the array into json 
-$jsondata=json_encode($formdata);
+$jsondata=json_encode($data);
+
+//save data into file
 file_put_contents("product.txt", $jsondata);
 
 // retrieve data from text file
@@ -21,25 +39,8 @@ $jsondata=file_get_contents("product.txt");
 //convert json into array
 $jsondata=json_decode($jsondata,true);
 
-// create an array and make json data as an element
-$newformdata[]=$jsondata;
-$newformdata[]=$formdata;
-
-// save the new form data into the product.text file as
-// a string
-
-file_put_contents(products.txt,json_decode($newformdata));
-
-// get the content out of the file for output
-json_decode(file_get_contents("product.txt"));
 }
-// function to validate input data
-function testdata($data){
-$data=trim($data);
-$data=stripslashes($data);
-$data=htmlspecialchars($data);
-return $data;
-}
+
 include "orderform.php";
 ?>
 
