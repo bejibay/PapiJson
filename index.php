48e809c06e9021168b689e57c345dc5317724c89
bejibay <58639255+bejibay@users.noreply.github.com>
2022-02-22 18:47:47 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head><title>PRODUCT$ ORDER DETAILS</title>
 <meta name="viewport" content="width=device-width,initial-scale=1.0">
-<link rel="stylesheet" href="orderproduct.css">
+<link rel="stylesheet" href="order.css">
 </head>
 <body>
 <h1>Product A</h1>
